e3e82859c06d11313339bbd2d93056fdf96e54e4
Josh Vincent <josh.vincent123+mac@gmail.com>
2020-12-17 15:39:15 +1100

    update

--- src/index.js
+++ src/index.js
@@ -11,6 +11,19 @@ import { CLIENT_ID, BASE_KEY } from './utils/localStorageInfo'
 import Amplify from '@aws-amplify/core'
 import config from './aws-exports'
 Amplify.configure(config)
+import Amplify, { Auth } from 'aws-amplify';
+import AWSAppSyncClient, { AUTH_TYPE } from 'aws-appsync';
+
+const getOIDCToken = async () => await 'token'; // Should be an async function that handles token refresh
+
+const client = new AWSAppSyncClient({
+  url: awsconfig.aws_appsync_graphqlEndpoint,
+  region: awsconfig.aws_appsync_region,
+  auth: {
+    type: AUTH_TYPE.OPENID_CONNECT,
+    jwtToken: () => getOIDCToken(),
+  },
+});
 
 const LOCAL_KEY = localStorage.getItem(BASE_KEY);
 
